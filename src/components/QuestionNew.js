406ddfe2d0e22e51c46150d63c150f997e2ebd3a
Eric Jones <eric.web.email@gmail.com>
2021-05-20 09:03:14 -0400

    Started Working On The New Question Component
    Started working on the new question component by adding the HTML for the form.  The form still requires some interactivity.

--- src/components/QuestionNew.js
+++ src/components/QuestionNew.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import Divider from "./Divider";
 
-const QuestionNew = (props) => {
-  return (
-    <h1>New Question</h1>
-  )
+
+class QuestionNew extends Component {
+
+  maxLength = 255
+
+  state = {
+    optionOne: "",
+    optionTwo: ""
+  }
+
+  render() {
+    return (
+      <div className="section">
+        <div className="container">
+          <div>
+            <h1 className="is-size-1 is-uppercase has-text-centered">Create New Question</h1>
+            <hr/>
+            <form>
+              <div className="field">
+                <label htmlFor="optionOne" className="label">Question 1</label>
+                <div className="control">
+                  <textarea name="optionOne" id="optionOne" cols="30" rows="10" className="textarea" />
+                </div>
+                <p className="help has-text-right">255 / {this.maxLength}</p>
+              </div>
+
+              <Divider title="or" />
+
+              <div className="field">
+                <label htmlFor="optionTwo" className="label">Question 2</label>
+                <div className="control">
+                  <textarea name="optionTwo" id="optionTwo" cols="30" rows="10" className="textarea" />
+                </div>
+                <p className="help has-text-right is-danger">255 / {this.maxLength}</p>
+              </div>
+
+              <div className="control mt-6">
+                <button className="button is-large is-primary is-fullwidth">Create</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
 }
 
 export default QuestionNew;
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 const Questions = (props) => {
   if (props.questions.length === 0) {
     return (
-      <article className="message is-info">
-        <div className="message-body has-text-info-dark">
+      <article className="message is-primary">
+        <div className="message-body has-text-primary-dark">
           <div className="block">
             <p className="is-size-1 is-info">Congratulations!</p>
             <p>You have answered all of the available questions.</p>
           </div>
-          <Link to="/questions/new" className="button is-info">Create Your Own Questions</Link>
+          <Link to="/questions/new" className="button is-primary">Create Your Own Questions</Link>
         </div>
       </article>
     )
